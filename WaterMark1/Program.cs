04ba354c2e4b521488baf12abe966d5f4fb06cac
bitddeveloper <bitddeveloper@gmail.com>
2020-06-15 13:14:41 +0300

    Create project and add library
    Signed-off-by: bitddeveloper <bitddeveloper@gmail.com>

---
+++ WaterMark1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using ConsoleAppFramework;
+using Microsoft.Extensions.Hosting;
+
+namespace WaterMark1
+{
+    class Program : ConsoleAppBase
+    {
+        public static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder().RunConsoleAppFrameworkAsync(args);
+        }
+    }
+}
